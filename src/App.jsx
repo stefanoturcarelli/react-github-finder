48f030c20a62157ffe110624fd55dabe2f8e8ca8
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-06-14 13:16:41 -0500

    remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Search from "./pages/Search.jsx";
 import User from "./pages/User.jsx";
